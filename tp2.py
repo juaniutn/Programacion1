4b6fdc7e1cd07a1147b85d4a3da287b6259b2e72
juaniutn <juan.lara@alumnos.frm.utn.edu.ar>
2023-08-28 22:28:28 -0300

    A

--- tp2.py
+++ tp2.py
@@ -35,19 +35,42 @@ elif (elecc=="b"):
 elif (elecc=="c"):
     print("Usted ha votado por el partido azul")
 else:
-    print("Ha ocurrido un error, verifique si eligio uno de los posibles candidatos")"""
+    print("Ha ocurrido un error, verifique si eligio uno de los posibles candidatos")
 
 #ej 5
 letra=input("Digite una letra para saber si es vocal o no ")
 vocal=["a", "e", "i", "o", "u"]
 letra=letra.lower()
-if (len(letra)>0):
+if (len(letra)==1):
     if (letra in vocal):
         print("Es una vocal!!")
     else:
         print("No es una vocal")
 else:
-    print("Ha ocurrido un error, digite una sola letra")
+    print("Ha ocurrido un error, digite una sola letra") 
+
+
+#ej 6
+año=int(input("Digite un año para saber si es bisiesto: "))
+if (año%4==0 and (año%100!=0 or año%400==0)):
+    print("Es un año bisiesto!")
+else:
+    print("No es un año bisiesto!")
+
+
+#ej 7
+n=input("Ingrese 3 numeros para saber su menor: ")
+if (n[0]>n[1] and n[0]>n[2]):
+    print(f"El numero mayor es", n[0])
+elif(n[1]>n[0] and n[1]>n[2]):
+    print(f"El numero mayor es", n[1])
+elif(n[2]>n[0] and n[2]>n[1]):
+    print(f"El numero mayor es", n[2])"""
+
+#ej 8
+
+    
+
 
     
 
