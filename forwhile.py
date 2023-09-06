c938e5fdd9902b8e20396c1a62a38fc0c5ed87c7
juaniutn <juan.lara@alumnos.frm.utn.edu.ar>
2023-09-06 15:14:56 -0300

    FORWHILE me habia confundido y subido un archivo que taba vacio perdon xd

--- forwhile.py
+++ forwhile.py
@@ -1,24 +1,49 @@
-"""Un grupo de amigos decide organizar un juego de estrategia, para lo cual forman dos equipos de 6 
-integrantes cada uno, donde un integrante de cada equipo es el “jefe” y los otros 5 son sus “oficiales”. 
-La regla más importante del juego es que sólo se comunicarán mediante un canal común, por lo que 
-deben buscar la forma de ocultar el contenido de sus mensajes. Uno de los equipos decide utilizar un 
-método antiguo de encriptación llamado “la cifra del césar”, que consiste en correr cada letra del 
-mensaje –considerando la posición de cada una en el alfabeto– una determinada cantidad de lugares. 
-Ejemplo: si el corrimiento es de 2 lugares, la palabra “ATAQUE” se transforma en “CVCSWG”.
-Cada día, el “jefe” del equipo debe enviar un mensaje a cada uno de sus oficiales.
-Escribir un programa que permita encriptar los 5 mensajes. El corrimiento (cantidad de lugares que se 
-correrán las letras) será dado por el usuario antes de comenzar a encriptar. Los 5 mensajes usarán el 
-mismo corrimiento.
-Nota: si el alfabeto termina antes de poder correr la cantidad de lugares necesarios, se vuelve a 
-comenzar desde la letra “a”.
-Ejemplo: la palabra “EXTRA” corrida 3 lugares se convierte en “HAWUD”. Utilizando el alfabeto 
-español, de 27 letras, el siguiente cálculo matemático permite volver a comenzar por el principio una 
-vez que se llegó a la “z”: (índice de la letra a correr+corrimiento)%27
-Sólo se encriptarán las letras de los mensajes, dejando al resto de caracteres sin modificación"""
 
-"""Crear un programa que solicite el ingreso de números enteros positivos, hasta que el usuario ingrese el 
-0. Por cada número, informar cuántos dígitos pares y cuántos impares tiene.
-Al finalizar, informar la cantidad de dígitos pares y de dígitos impares leídos en total"""
-num=input("Ingrese un numero posiivo")
+#1
+abc=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+word=input("Escriba su palabra a encriptar: ")
+word=word.lower()
+long=int(input("Cuantos lugares quiere correr en su encriptado?: "))
+longword=len(word)
+for i in range(0,longword,1):
+    if(word.isalpha()):
+        position=abc.index(word[i])
+        showedword=abc[(position+long)%27]
+        print(f"{showedword}", end="")
+    else:
+        print(word[i])
 
+
+#2
+num=int(input("Ingrese un numero positivo: "))
+numpair=0
+numimpair=0
+numpairtotal=0
+numimpairtotal=0
 while (num!=0):
+    nums=str(num)
+    long=len(nums)
+    for i in range(0,long, 1):
+        numif=nums[i]
+        numif=int(numif)
+        if(numif==0):
+            numpair=numpair+1
+        elif(numif%2==0):
+            numpair=numpair+1
+        elif(numif%2!=0):
+            numimpair=numimpair+1
+    numpairtotal=numpairtotal+numpair
+    numimpairtotal=numimpair+numimpairtotal
+    print ("Cantidad de numeros impares: ", numimpair)
+    print (f"Cantidad de numeros pares: {numpair}")
+    numpair=0
+    numimpair=0
+    num=int(input("Ingrese un numero posiivo: "))
+    
+print(f"La cantidad total de numeros pares es: {numpairtotal}")
+print(f"La cantidad total de numeros impares es: {numimpairtotal}")
+
+    
+
+
+    
