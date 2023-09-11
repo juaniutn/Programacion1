2bdb015b56341af01d6dda5c3b11e1cc0bbb599e
juaniutn <juan.lara@alumnos.frm.utn.edu.ar>
2023-09-11 10:49:48 -0300

    tp 4

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+while True:
+    number=int(input("En que menu desea ingresar? 1 2 o 3, ingrese 0 para salir: "))
+    if(number==1):
+        print("MENU UNO")
+    elif(number==2):
+        print("MENU DOS")
+    elif(number==3):
+        print("MENU TREIS")
+    elif(number==0):
+        break
